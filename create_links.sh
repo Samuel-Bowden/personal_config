51c6a464bf9773537393a6118c6dbf418410555c
Sam Bowden <srb55@student.le.ac.uk>
2023-02-04 20:59:54 +0000

    Add sway config and linking script, change alacritty to nord

---
+++ create_links.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/bash
+
+read -p "Enter name of configuration profile: " profile
+
+case $profile in
+  "desktop")
+    name="desktop"
+    ;;
+
+  "laptop")
+    name="laptop"
+    ;;
+
+  *)
+    echo "Invalid profile name"
+    exit 1
+    ;;
+esac
+
+echo "Creating links for $name profile:"
+
+echo "alacritty"
+ln alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
+
+echo "helix"
+ln helix/config.toml ~/.config/helix/config.toml
+
+echo "mangohud"
+ln mangohud/$name/MangoHud.conf ~/.config/MangoHud/MangoHud.conf
+
+echo "sway"
+ln sway/$name/config ~/.config/sway/config
